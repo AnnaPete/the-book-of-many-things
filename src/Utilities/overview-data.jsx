ef1847aa826719142544475d835b6b9cbd88c5e5
Anna Peterson <reveragb@gmail.com>
2023-03-02 13:52:42 -0700

    Add data

---
+++ src/Utilities/overview-data.jsx
@@ -0,0 +1,76 @@
+export const overview = 
+{
+  rules: 
+    [
+      "Character Creation: To play D&D, you will need to create a character. This involves choosing a race, class, and background for your character, as well as assigning values to six ability scores (strength, dexterity, constitution, intelligence, wisdom, and charisma). Your character\'s abilities and skills will be determined by these scores.", 
+
+      "Ability Checks: During the game, you will need to roll a 20-sided die (also known as a d20) to determine the success of your character\'s actions. The DM will assign a target number (known as a Difficulty Class or DC) based on the difficulty of the action, and you will need to roll equal to or higher than that number to succeed.", 
+
+      "Combat: When your character is in combat, you will need to roll for initiative to determine when your turn comes up. During your turn, you can take various actions, such as attacking an enemy, casting a spell, or using an item. You will need to roll a d20 to determine if your attack hits, and if it does, you will roll damage based on your weapon or spell.", 
+
+      "Spellcasting: If your character is a spellcaster, you will have access to a variety of spells with different effects. To cast a spell, you will need to use up one of your spell slots (which replenish after a rest), and then make a spell attack roll or have the target make a saving throw against your spell's DC.", 
+
+      "Experience Points: As you play the game and complete adventures, your character will earn experience points (XP). Once you earn enough XP, your character will level up, which allows you to improve your abilities, gain new spells or abilities, and become more powerful overall.", 
+
+      "These are just the basics of D&D, but they should give you a good starting point for understanding the game. The most important thing is to have fun and be creative!"
+    ],
+
+  races: 
+    [
+      "Dungeons & Dragons (D&D) races are essentially the different humanoid or semi-humanoid species that a player can choose to play as in the game. Each race has unique characteristics, such as physical traits, cultural background, and special abilities.", 
+
+      "Some common races in D&D include humans, elves, dwarves, halflings, and orcs. Each race has its own strengths and weaknesses, and some may be better suited for certain classes or roles within the game. The choice of race can also affect how the character is perceived and treated by non-player characters within the game world."
+    ],
+
+  classes: 
+    [
+      "Dungeons & Dragons (D&D) classes refer to the different roles or archetypes that a player can choose to take on within the game. Here's a brief overview of each class:", 
+
+      "Barbarian: A fierce warrior who relies on brute strength and primal rage to overpower enemies.", 
+
+      "Bard: A versatile performer who uses magic and music to inspire allies, control enemies, and heal wounds.", 
+
+      "Cleric: A divine spellcaster who draws power from a deity to heal allies, harm enemies, and support the party in a variety of ways.", 
+
+      "Druid: A nature-based spellcaster who can shapeshift into animals, call upon natural forces, and manipulate the environment.", 
+
+      "Fighter: A skilled warrior who excels in combat, with a wide range of abilities and weapon proficiencies.", 
+
+      "Monk: A disciplined martial artist who can harness their own ki energy to perform incredible feats of strength, agility, and speed.", 
+
+      "Paladin: A holy warrior who channels divine power to protect and support the party, while smiting foes with righteous fury.", 
+
+      "Ranger: A skilled tracker and survivalist who can navigate through any environment, hunt prey, and cast spells to aid allies or hinder enemies.", 
+
+      "Rogue: A stealthy thief or assassin who excels at sneaking, picking locks, disarming traps, and dealing massive damage with surprise attacks.", 
+
+      "Sorcerer: A spellcaster who has innate magical abilities, drawing upon a limited number of spells known but with great flexibility in casting.", 
+
+      "Warlock: A spellcaster who has made a pact with a powerful entity (such as a demon or a deity) to gain magical abilities and otherworldly powers.", 
+
+      "Wizard: A studious spellcaster who has mastered a vast array of spells and can cast them using a spellbook.", 
+
+      "Each class has its own strengths and weaknesses, so it's important to choose one that fits your play style and the needs of your party."
+    ],
+
+  backgrounds: 
+    [
+
+    ],
+
+  ability_scores: 
+    [
+
+    ],
+
+  skills: 
+    [
+
+    ],
+
+  proficiencies: 
+    [
+      
+    ]
+
+}
