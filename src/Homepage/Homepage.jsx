4259824ff80e1e9e7cde29e17cee8adcd55ebf25
Anna Peterson <reveragb@gmail.com>
2023-03-02 10:01:34 -0700

    Add p tags for better readability

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -9,9 +9,9 @@ const Homepage = () => {
       <div className='homepage-body'>
         <section className='intro'>
           <h3>What exactly IS Dungeons & Dragons?</h3>
-          <p>
-            Dungeons & Dragons (D&D) is a fantasy tabletop role-playing game where players create their own characters and go on adventures guided by a storyteller, called the Dungeon Master (DM). The game involves rolling dice to determine the outcome of various actions and decisions made by the players. D&D allows for a great deal of creativity and imagination as players work together to solve puzzles, fight monsters, and explore fantastical worlds. It's a collaborative storytelling experience where anything can happen and the possibilities are endless.
-          </p>
+          <p>Dungeons & Dragons (D&D) is a fantasy tabletop role-playing game where players create their own characters and go on adventures guided by a storyteller, called the Dungeon Master (DM).</p>
+          <p>The game involves rolling dice to determine the outcome of various actions and decisions made by the players. D&D allows for a great deal of creativity and imagination as players work together to solve puzzles, fight monsters, and explore fantastical worlds.</p>
+          <p>It's a collaborative storytelling experience where anything can happen and the possibilities are endless.</p>
         </section>
         <img src={image} alt='adventurers fighting a dragon'/>
       </div>
