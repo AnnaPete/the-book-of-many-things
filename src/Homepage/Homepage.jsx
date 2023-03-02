26918cc167e2a841d9b2d75567b689b1765fa4a3
Anna Peterson <reveragb@gmail.com>
2023-03-02 09:19:04 -0700

    Create basic Homepage

---
+++ src/Homepage/Homepage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Homepage.css'
+import image from '../Images/dungeons-and-dragons-lede.jpg'
+
+const Homepage = () => {
+  return (
+    <div className='homepage-container'>
+      <h2 className='greeting'>Welcome to the Book of Many Things - your own personal Dungeons & Dragons encyclopedia!</h2>
+      <div className='homepage-body'>
+        <section className='intro'>
+          <h3>What exactly IS Dungeons & Dragons?</h3>
+          <p>
+            Dungeons & Dragons (D&D) is a fantasy tabletop role-playing game where players create their own characters and go on adventures guided by a storyteller, called the Dungeon Master (DM). The game involves rolling dice to determine the outcome of various actions and decisions made by the players. D&D allows for a great deal of creativity and imagination as players work together to solve puzzles, fight monsters, and explore fantastical worlds. It's a collaborative storytelling experience where anything can happen and the possibilities are endless.
+          </p>
+        </section>
+        <img src={image} alt='adventurers fighting a dragon'/>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
