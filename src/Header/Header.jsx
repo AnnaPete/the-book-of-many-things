7e21663b8d33df197b72290b1badbafe1c0c22ec
Anna Peterson <reveragb@gmail.com>
2023-03-03 11:43:32 -0700

    Add a method to create navlinks

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,20 @@
 import './Header.css'
 import React from 'react'
-import { Route } from 'react-router-dom'
-import Overview from '../Overview/Overview'
 import title from '../Images/title.png'
-// import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
-function Header() {
+const Header = ( { data } ) => {
+
+  const renderLinks = () => {
+    if (!data) {
+      return null
+    }
+
+    return Object.keys(data).map((key) => (
+      <NavLink to={`/${key}`} key={key} >{key}</NavLink>
+    ))
+  }
+
   return (
     <header className='header'>
       <div className='logo-container'>
@@ -14,22 +23,7 @@ function Header() {
       <div className='title-container'>
         <h1 className='title'>The Book of Many Things</h1>
         <nav>
-          <button className='rules'>Rules</button>
-          <button className='races'>Races</button>
-          <button className='classes'>Classes</button>
-          <button className='alignments'>Alignments</button>
-          <button className='backgrounds'>Backgrounds</button>
-          <button className='ability-scores'>Ability Scores</button>
-          <button className='skills'>Skills</button>
-          <button className='proficiencies'>Proficiencies</button>
-          {/* <Route path='/basic-rules' render={ ()=> <Overview className='rules' /> } />
-          <Route path='/races' render={ ()=> <Overview className='races' /> } />
-          <Route path='/classes' render={ ()=> <Overview className='classes' /> } />
-          <Route path='/alignments' render={ ()=> <Overview className='alignments' /> } />
-          <Route path='/backgrounds'  render={ ()=> <Overview className='backgrounds' /> } />
-          <Route path='/ability-scores'  render={ ()=> <Overview className='ability-scores' /> } />
-          <Route path='/skills' render={ ()=> <Overview className='skills' /> } />
-          <Route path='/proficiencies'  render={ ()=> <Overview className='proficiencies' /> } /> */}
+          {renderLinks()}
         </nav>
       </div>
     </header>
