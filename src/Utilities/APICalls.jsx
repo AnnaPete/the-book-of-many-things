329d46e9f476def9b24be483a136c8040ecbc966
Anna Peterson <reveragb@gmail.com>
2023-03-03 11:41:19 -0700

    Fix CORS issue

--- src/Utilities/APICalls.jsx
+++ src/Utilities/APICalls.jsx
@@ -1,15 +1,12 @@
 export const getData = () => {
-  paths.forEach(path => {
-    return (
-      fetch('http://localhost:3000/api/v1/data')
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        } else {
-          return Promise.reject(response.status)
-        }
-      })
-    )
+  return fetch('https://thebookofmanythings.herokuapp.com/api/v1/data', {
+    mode: 'no-cors'
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      return Promise.reject(response.status)
+    }
   })
 }
-
