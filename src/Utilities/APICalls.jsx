b8cd4e931e1e7f5ea8a8178cb259c828edc4590e
Anna Peterson <reveragb@gmail.com>
2023-03-02 19:04:15 -0700

    Change API

--- src/Utilities/APICalls.jsx
+++ src/Utilities/APICalls.jsx
@@ -1,18 +1,7 @@
-const rules = '/rules'
-const races = '/races'
-const classes = '/classes'
-const abilityScores = '/ability-scores'
-const alignments = '/alignments'
-const backgrounds = '/backgrounds'
-const proficiencies = '/proficiencies'
-const skills  = '/skills'
-const spells = '/spells'
-const paths = [rules, races, classes, abilityScores, alignments, backgrounds, proficiencies, skills, spells]
-
 export const getData = () => {
   paths.forEach(path => {
     return (
-      fetch(`https://www.dnd5eapi.co/api${path}`)
+      fetch('http://localhost:3000/api/v1/data')
       .then(response => {
         if (response.ok) {
           return response.json()
