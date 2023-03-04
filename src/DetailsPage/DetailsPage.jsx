c4d68468a5a314627ebca9cbfe979552b11119d8
Anna Peterson <reveragb@gmail.com>
2023-03-04 13:04:38 -0700

    Add method to render details to the page

--- src/DetailsPage/DetailsPage.jsx
+++ src/DetailsPage/DetailsPage.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import details from '../Utilities/details-data'
+import raid from '../Images/dungeon-raid.png'
 import './DetailsPage.css'
 
-function DetailsPage({ item }) {
+function DetailsPage({ details }) {
 
-  
+  const displayDetails = () => {
+    return details.map((item, i) => {
+      return (
+        <p key={i}>{ item }</p>
+      )
+    })
+  }
 
-  if (item === details[item])
   return (
-    <h1>Yo</h1>
+    <div className='details'>
+      <div className='details-body'>
+        <img src={raid} alt='adventurers fighting undead'/>
+        <div className='details-text'>
+          <h1>More Info</h1>
+          {displayDetails()}
+        </div>
+      </div>
+    </div>
   )
 }
-
 
 export default DetailsPage
